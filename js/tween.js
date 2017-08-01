0f9a5393ad1a797d8e6db9ff896f2e7734667231
sexfat <bryant6831@gmail.com>
2017-08-01 23:10:44 +0800

    update

--- js/tween.js
+++ js/tween.js
@@ -1,63 +1,63 @@
-$(function() {
+$(function () {
 
-//====================
-//選單
-//====================
-
-
-$('a.page-scroll').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-              $('html,body').animate({
-                  scrollTop: target.offset().top
-              }, 1000);
-              return false;
-          }
-      }
-  });
-  console.log('nav ok');
+    //====================
+    //選單
+    //====================
 
 
-  /* ==============================================
-       回到最頂端
+    $('a.page-scroll').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 800);
+                return false;
+            }
+        }
+    });
+    console.log('nav ok');
+
+
+    /* ==============================================
+         回到最頂端
+         =============================================== */
+    $(window).scroll(function () {
+        var offset_height = 400;
+        var distance = $(window).scrollTop();
+
+        if (distance > offset_height) {
+            $('.gototop').addClass('on');
+        } else {
+            $('.gototop').removeClass('on');
+        }
+    });
+
+    $('.gototop').on('click', function (e) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0,
+        }, 700);
+        console.log('gototop');
+    });
+
+
+    /* ==============================================
+        選單出現
        =============================================== */
-     $(window).scroll(function(){
-       var offset_height = 400;
-       var distance =$(window).scrollTop();
-
-       if (distance > offset_height){
-          $('.gototop').addClass('on');
-       }else {
-         $('.gototop').removeClass('on');
-       }
-     });
-
-     $('.gototop').on('click',function(e){
-       event.preventDefault();
-       $('body,html').animate({
-         scrollTop: 0,
-       },700);
-       console.log('gototop');
-     });
 
 
-     /* ==============================================
-         選單出現
-        =============================================== */
+    $(window).scroll(function () {
+        var offset_height = 700;
+        var distance = $(window).scrollTop();
 
-
-        $(window).scroll(function(){
-          var offset_height = 700;
-          var distance =$(window).scrollTop();
-
-          if (distance > offset_height){
-             $('.navbar').addClass('on');
-          }else {
+        if (distance > offset_height) {
+            $('.navbar').addClass('on');
+        } else {
             $('.navbar').removeClass('on');
-          }
-        });
+        }
+    });
 
 
 
@@ -66,7 +66,7 @@ $('a.page-scroll').click(function() {
 
 
 
-//第一個場景動畫
+    //第一個場景動畫
     TweenMax.fromTo(".section_01 .title", 2, {
         y: -150,
         opacity: 0,
@@ -90,6 +90,7 @@ $('a.page-scroll').click(function() {
     console.log("ok");
     //scrollmagic start
 
+
     var controller = new ScrollMagic.Controller();
     // new 場景
 
@@ -107,7 +108,7 @@ $('a.page-scroll').click(function() {
     var scene = new ScrollMagic.Scene({
             triggerElement: "#section_01",
             reverse: true,
-            offset:'100px',
+            offset: '200px',
             // duration: '90%'
         })
 
@@ -116,90 +117,90 @@ $('a.page-scroll').click(function() {
         .addTo(controller);
     //  add css
 
+
     var ourScene = new ScrollMagic.Scene({
-        triggerElement: "#section_02",
-        offset:'200px'
-    })
-     .setClassToggle('.icon','fade-in')
-     .addIndicators({name :'bgc start'})
-     .addTo(controller);
+            triggerElement: "#section_02",
+            offset: '100px'
+        })
+        .setClassToggle('.icon', 'fade-in')
+        .addIndicators()
+        .addTo(controller);
 
 
-// 改變背景顏色
-var bgc_sence = new ScrollMagic.Scene({
-    triggerElement: "#section_03",
-    offset:'100px'
-})
- .setClassToggle('.bgc','color_fadein')
- .addIndicators({
-    name :'bgc start'
- })
- .addTo(controller);
 
-//sticky box
 
-var scene_sticky = new ScrollMagic.Scene({
-  triggerElement: "#section_04",
-  duration: 200,
-  offset: 10
-})
+    // 改變背景顏色
+    var bgc_sence = new ScrollMagic.Scene({
+            triggerElement: "#section_03",
+            offset: '100px'
+        })
+        .setClassToggle('.bgc', 'color_fadein')
+        .addIndicators({
+            name: 'bgc start'
+        })
+        .addTo(controller);
 
-.setPin("#sticky" ,{spacerClass: 'asd'})
-.addIndicators({
-   name :'sticky start'
-})
-.addTo(controller);
 
-//移動字
+    //sticky box
+    var scene_sticky = new ScrollMagic.Scene({
+            triggerElement: "#section_04",
+            offset: '100',
+            duration: '800'
+        })
+        .setPin('#sticky')
+        .addIndicators()
+        .addTo(controller);
 
-var scene_move = new ScrollMagic.Scene({
 
-  triggerElement: "#section_05",
-  duration: 400,
-  reverse: true,
-})
+    //移動字
 
-  .setTween(TweenMax.staggerTo(".section_06 .title" , 1 ,{
-      opacity: 1,
-      x : 1350,
-      ease: Strong.easeInOut
-
-  }))
-  .addIndicators({
-     name :'move'
-  })
-.addTo(controller);
-
-console.log('move ok');
-//stellar init
-$('#container').stellar({
+    var scene_move = new ScrollMagic.Scene({
+            triggerElement: "#section_05",
+            offset: '400',
+            duration: '600',
+            reverse: true
+        })
+        .setTween(TweenMax.staggerTo('.section_06 .title', 1, {
+            x: 1350,
+            ease: Strong.easeInOut
+        }))
+        .addIndicators()
+        .addTo(controller);
+    console.log('move ok');
+  
+  //stellar
+  $('#container').stellar({
   horizontalOffset: 0,
   verticalOffset: 0,
 });
 
 
 
-});
 
-//Timelinemax
-$(function(){
-var tl = new TimelineMax(),
-    box = $('.animation_box'),
-    circle = $('.animation_circle'); 
-    
-
-tl.to( box ,0.5 ,{left: 100})
-  .to(circle ,0.5 ,{top: 200});
-  console.log('time ok');
-});
-
-$(function(){
-   
-    var circle_box = $('.circle');
-    
-    TimelineMax.staggerTo( circle_box , 1 , { left :  });
-    console.log('timeline ok');
 });
 
 
 
+
+
+
+
+// //Timelinemax
+// $(function(){
+// var tl = new TimelineMax(),
+//     box = $('.flow .item');
+//     // circle = $('.animation_circle'); 
+
+
+// tl.to( box ,0.5 ,{left: 100});
+// //   .to(circle ,0.5 ,{top: 200});
+//   console.log('time ok');
+// });
+
+// $(function(){
+
+//     var circle_box = $('.circle');
+
+//     TimelineMax.staggerTo( circle_box , 1 , { left :  });
+//     console.log('timeline ok');
+// });
